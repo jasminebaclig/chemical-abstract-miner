3bf9ee3424ec72ea1c9ddde8f0b5f84c0b24581d
jasminebaclig <jasminebaclig@yahoo.com>
2023-09-21 14:07:53 -0600

    Progressed through compound_looker method

--- compound_looker.R
+++ compound_looker.R
@@ -18,7 +18,11 @@ compound_looker <- function(abs_param, id_param) {
   abs_param <- gsub(":", "", abs_param)
   abs_param <- gsub(";", "", abs_param)
   abs_param <- gsub("\\(", "", abs_param)
-  abs_param <- gsub("\\)", "", abs_param) %>% tolower()
+  abs_param <- gsub("\\)", "", abs_param)
+  abs_param <- gsub("\"", "", abs_param)
+  abs_param <- gsub("\\[", "", abs_param)
+  abs_param <- gsub("\\]", "", abs_param)
+  abs_param <- gsub("\\/", "", abs_param) %>% tolower()
   
   split_abs <- strsplit(abs_param, split = " ") #Splits each abstract into individual words
   
@@ -28,5 +32,31 @@ compound_looker <- function(abs_param, id_param) {
   if(!exists("element_names") || !exists("combined_prefixes") || !exists("suffixes")) {
     source("nomenclature_csv_maker.R")
     nomenclature_csv_maker()
+  }
+  
+  
+  #FIRST ITERATION: Removes strings with no letters and combines two-word chemical names
+  no_letters <- c()
+  two_word_end <- c()
+  two_word_start <- c()
+  
+  for(i in 1:length(split_abs)) {
+    for(j in 1:length(split_abs[[i]])) {
+      string <- split_abs[[i]][j]
+      
+      if(!grepl("[A-Za-z]", string)) {
+        no_letters <- append(no_letters, j)
+      }
+      
+      if(string == "acid") {
+        two_word_end <- append(two_word_end, j)
+      }
+      
+      if(string %in% element_names) {
+        two_word_start <- append(two_word_start, j)
+      }
+      
+      
+    }
   }
 }
