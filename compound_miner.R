4a88f510d1adde67231e772edbb656898cd08a12
jasminebaclig <jasminebaclig@yahoo.com>
2023-10-18 07:16:18 -0600

    Adds for-loop functionality

--- compound_miner.R
+++ compound_miner.R
@@ -2,32 +2,47 @@
 library(devtools)
 library(RISmed)
 library(tidyverse)
-library(dictionaRy)
 
 install_github("BuerkiLabTeam/G2PMineR")
 library(G2PMineR)
 
 
 
-#Runs literature search on plant species and retrieves abstracts
-query_result <- EUtilsSummary("\"Abies balsamea\"", type = "esearch", db = "pubmed", retmax = 10000)
-id <- attr(query_result, "PMID") %>% as.numeric()
-abstracts <- AbstractsGetteR(id)
+#Imports plant species list for given animal
+species_list <- read_csv("caribou_species_list.csv") %>%
+                select("Species name")
 
 
 
-#Cleans up abstracts
-missing <- c() #Contains indexes with missing abstracts
+#Prepares output file
+output <- data.frame(species = character(), pmid = character())
 
-for(i in 1:length(abstracts)) {
-  if(is.na(abstracts[i])) {
-    missing <- append(missing, i)
+
+
+for(i in 1:length(species_list)) {
+  #Runs literature search on plant species and retrieves abstracts
+  search_string <- paste("\"", species_list[[1]][i], "\"", sep = "")
+  query_result <- EUtilsSummary(search_string, type = "esearch", db = "pubmed", retmax = 10000)
+  id <- attr(query_result, "PMID") %>% as.numeric()
+  abstracts <- AbstractsGetteR(id)
+  
+  
+  
+  #Removes PMID with missing abstracts
+  missing <- c() #Contains indexes with missing abstracts
+  
+  for(i in 1:length(abstracts)) {
+    if(is.na(abstracts[i])) {
+      missing <- append(missing, i)
+    }
   }
+  
+  if(!is.null(missing)) {
+    id <- id[-missing]
+  }
+  
+  
+  
+  #Concatenates PMID
+  pmid <- paste(id, collapse = "\t")
 }
-
-if(!is.null(missing)) {
-  abstracts <- abstracts[-missing] #Removes missing abstracts and respective id's
-  id <- id[-missing]
-}
-
-abstracts <- HTMLElementRemoveR(abstracts) #Removes HTML elements from abstracts
