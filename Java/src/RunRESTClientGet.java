e9e01941802ea76427029a7f79f359a0c826dfcc
jasminebaclig <jasminebaclig@yahoo.com>
2023-10-23 16:16:34 -0600

    Finishes code

--- Java/src/RunRESTClientGet.java
+++ Java/src/RunRESTClientGet.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 
 public class RunRESTClientGet {
     public static void main(String[] args) {
-        Scanner s = null;
         String[] param = {"Chemical", "PUBIDs", "PubTator"};
         String[] currLine;
 
         try {
-            File inputFile = new File("pmid.txt");
-            File pmidList = new File("pmidList.txt");
-            File outputFile = new File("output.txt");
-            File chemicalFile = new File("chemical.txt");
+            File inputFile = new File("pmid.txt"); //Input file with list of pmid for each species
+            File pmidList = new File("pmidList.txt"); //Pmid list for one specie at a time
+            File outputFile = new File("output.txt"); //Output of RESTClientGet.java
+            File chemicalFile = new File("chemical.txt"); //Output file with chemical names for each species
             pmidList.createNewFile();
             outputFile.createNewFile();
             chemicalFile.createNewFile();
@@ -19,16 +18,22 @@ public class RunRESTClientGet {
             PrintStream ps = new PrintStream(outputFile);
             System.setOut(ps);
 
-            s = new Scanner(inputFile);
-            
+            Scanner s = new Scanner(inputFile);
             BufferedWriter bw = new BufferedWriter(new FileWriter(pmidList));
+
+            while(s.hasNextLine()) {
+                currLine = s.nextLine().split("\t"); //String array with species name followed by pmid
+
+                for(int i = 1; i < currLine.length; i++) {
+                    bw.write(currLine[i] + "\n");
+                }
+
+                param[1] = "pmidList.txt";
+                RESTClientGet.main(param);
+                processOutputFile(outputFile, chemicalFile);
+            }
         } catch(IOException e) {
             System.exit(1);
-        }
-
-        while(s.hasNextLine()) {
-            currLine = s.nextLine().split("\t");
-            RESTClientGet.main(param);
         }
     }
 
--- compound_miner.R
+++ compound_miner.R
@@ -3,9 +3,6 @@ library(devtools)
 library(RISmed)
 library(tidyverse)
 
-install_github("BuerkiLabTeam/G2PMineR")
-library(G2PMineR)
-
 
 
 #Imports plant species list for given animal
@@ -19,27 +16,11 @@ output <- data.frame(species = character(), pmid = character())
 
 
 
-for(i in 1:length(species_list)) {
-  #Runs literature search on plant species and retrieves abstracts
+for(i in 1:length(species_list[[1]])) {
+  #Runs literature search on plant species and retrieves PMID
   search_string <- paste("\"", species_list[[1]][i], "\"", sep = "")
   query_result <- EUtilsSummary(search_string, type = "esearch", db = "pubmed", retmax = 10000)
   id <- attr(query_result, "PMID") %>% as.numeric()
-  abstracts <- AbstractsGetteR(id)
-  
-  
-  
-  #Removes PMID with missing abstracts
-  missing <- c() #Contains indexes with missing abstracts
-  
-  for(i in 1:length(abstracts)) {
-    if(is.na(abstracts[i])) {
-      missing <- append(missing, i)
-    }
-  }
-  
-  if(!is.null(missing)) {
-    id <- id[-missing]
-  }
   
   
   
