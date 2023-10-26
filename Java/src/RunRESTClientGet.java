ea0b5dfbc36dedf59c5fec24d11f697611b8e0cf
jasminebaclig <jasminebaclig@yahoo.com>
2023-10-25 21:49:13 -0600

    Finishes Java code

--- Java/src/RunRESTClientGet.java
+++ Java/src/RunRESTClientGet.java
@@ -1,43 +1,56 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class RunRESTClientGet {
     public static void main(String[] args) {
-        String[] param = {"Chemical", "PUBIDs", "PubTator"};
+        File inputFile = new File("pmid.txt"); //Input file with list of pmid for each species
+        File pmidList = new File("pmidList.txt"); //Pmid list for one specie at a time
+        File outputFile = new File("output.tsv"); //Output of RESTClientGet.java
+        File chemicalFile = new File("chemical.txt"); //Output file with chemical names for each species
+
         String[] currLine;
+        String[] param = {"Chemical", "pmidList.txt", "PubTator"};
+
+        int count = 0;
 
         try {
-            File inputFile = new File("pmid.txt"); //Input file with list of pmid for each species
-            File pmidList = new File("pmidList.txt"); //Pmid list for one specie at a time
-            File outputFile = new File("output.txt"); //Output of RESTClientGet.java
-            File chemicalFile = new File("chemical.txt"); //Output file with chemical names for each species
             pmidList.createNewFile();
-            outputFile.createNewFile();
             chemicalFile.createNewFile();
 
-            PrintStream ps = new PrintStream(outputFile);
-            System.setOut(ps);
-
             Scanner s = new Scanner(inputFile);
-            BufferedWriter bw = new BufferedWriter(new FileWriter(pmidList));
+            PrintStream stdOut = System.out;
 
             while(s.hasNextLine()) {
                 currLine = s.nextLine().split("\t"); //String array with species name followed by pmid
+                BufferedWriter bw = new BufferedWriter(new FileWriter(pmidList, false));
 
                 for(int i = 1; i < currLine.length; i++) {
                     bw.write(currLine[i] + "\n");
                 }
 
-                param[1] = "pmidList.txt";
+                outputFile.createNewFile();
+                PrintStream ps = new PrintStream(outputFile);
+                System.setOut(ps);
+
                 RESTClientGet.main(param);
-                processOutputFile(outputFile, chemicalFile);
+                processOutputFile(outputFile, chemicalFile, currLine[0]);
+
+                System.setOut(stdOut);
+                outputFile.delete();
+                bw.close();
+
+                count++;
+                System.out.println(count + " species done.");
             }
+
+            s.close();
         } catch(IOException e) {
             System.exit(1);
         }
     }
 
-    private static void processOutputFile(File outputFile, File chemicalFile) {
+    private static void processOutputFile(File outputFile, File chemicalFile, String speciesName) {
         Scanner s = null;
 
         try {
@@ -48,15 +61,37 @@ public class RunRESTClientGet {
         
         String currLine = "";
         String[] stringArray;
+        ArrayList<String> chemicalArray = new ArrayList<>();
 
         while(s.hasNextLine()) {
             currLine = s.nextLine();
 
-            if(currLine.contains("|t|") || currLine.contains("|a|")) {
+            if(currLine.contains("|t|") || currLine.contains("|a|") || currLine.isBlank()) {
                 continue;
             } else {
-                stringArray = currLine.split("  ");
+                stringArray = currLine.split("\t");
+
+                if(!chemicalArray.contains(stringArray[3])) {
+                    chemicalArray.add(stringArray[3]);
+                }
             }
+        }
+
+        s.close();
+        String outputLine = speciesName + "\t";
+
+        for(String chemical : chemicalArray) {
+            outputLine += chemical + ", ";
+        }
+
+        outputLine = outputLine.substring(0, outputLine.length() - 2);
+
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile, true));
+            bw.write(outputLine + "\n");
+            bw.close();
+        } catch(IOException e) {
+            System.exit(1);
         }
     }
 }
