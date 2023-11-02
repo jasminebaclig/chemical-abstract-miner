886324d8413723ddc9dec61310fcdccfd91e45c5
jasminebaclig <jasminebaclig@yahoo.com>
2023-11-02 15:16:22 -0600

    Worked on post-analysis

--- post_java_analysis.R
+++ post_java_analysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(RISmed)
 
 results <- read_tsv("caribou_chemicals.tsv", col_names = TRUE) #36207
 
@@ -8,8 +9,17 @@ elements <- read_csv("pte.csv", col_names = TRUE) %>%
 results_elem <- filter(results, chemical %in% elements$Element | chemical %in% elements$Symbol) #1670
 results <- filter(results, !(chemical %in% elements$Element) & !(chemical %in% elements$Symbol)) #34537
 
-amino_acids <- read_csv("amino-acids.csv", col_names = TRUE) %>%
-               select("full name", "three letter code", "single letter code") %>%
+amino_acids <- read_csv("amino-acids.csv", col_names = TRUE)
+amino_acids <- select(amino_acids, "full name", "three letter code", "single letter code") %>%
                transform("full name" = tolower(amino_acids$"full name"), "three letter code" = tolower(amino_acids$"three letter code"), "single letter code" = tolower(amino_acids$"single letter code"))
 results_aa <- filter(results, chemical %in% amino_acids$full.name | chemical %in% amino_acids$three.letter.code | chemical %in% amino_acids$single.letter.code) #360
 results <- filter(results, !(chemical %in% amino_acids$full.name) & !(chemical %in% amino_acids$three.letter.code) & !(chemical %in% amino_acids$single.letter.code)) #34177
+
+cid_table <- data.frame(chemical = character(), cid = character(), count = numeric())
+for(i in 1:length(results)) {
+  search_string <- paste("\"", results$chemical[i], "\"", sep = "")
+  Sys.sleep(0.05)
+  query_result <- EUtilsSummary(search_string, type = "esearch", db = "pccompound", retmax = 10000)
+  new_row <- c(results$chemical[i], attr(query_result, "PMID"), attr(query_result, "count"))
+  cid_table <- rbind(cid_table, new_row)
+}
